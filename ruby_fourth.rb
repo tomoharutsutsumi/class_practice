87f507d734ecd8cc590a053c9943e61f683b4da1
tomoharu tsutsumi <>
2018-07-01 15:53:43 +0900

    add kkkk......以下　random_connect追加

--- ruby_fourth.rb
+++ ruby_fourth.rb
@@ -168,4 +168,82 @@ connector.delim.push(";")
 
 
 
+p "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk"
+
+class Connector
+
+  attr_accessor :delim
+
+  def initialize
+    @connector = []
+    @delim = []
+  end
+
+  def push(str)
+    @connector.push(str)
+  end
+
+  def random_connect
+    connector =  @connector.shuffle
+    last = ""
+    connector.each_with_index do |connector, index|
+    index += 1
+    last += index.to_s
+    last += "."
+      if index == @connector.size
+        last += connector
+      else
+        if index % 2 != 0
+          last += connector
+          last += "#{@delim[0]}"
+        elsif index % 2 == 0
+          last += connector
+          last += "#{@delim[1]}"
+        end
+      end
+    end
+    last
+  end
+
+  def connect
+    last = ""
+    @connector.each_with_index do |connector, index|
+    index += 1
+    last += index.to_s
+    last += "."
+      if index == @connector.size
+        last += connector
+      else
+        if index % 2 != 0
+          last += connector
+          last += "#{@delim[0]}"
+        elsif index % 2 == 0
+          last += connector
+          last += "#{@delim[1]}"
+        end
+      end
+    end
+    last
+  end
+end
+
+
+
+connector = Connector.new
+connector.push("suzuki")
+connector.push("tanaka")
+connector.push("yyyyyy")
+connector.push("hhhhhh")
+connector.push("wwwwwwwwwww")
+connector.push("tttttttttttttttt")
+connector.delim.push(",")
+connector.delim.push(";")
+ p connector.connect
+ p connector.random_connect
+ p connector.connect
+
+
+
+
+
 
